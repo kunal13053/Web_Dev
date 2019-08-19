0a737177416a5d1f678157ec10cd21e171e62b2a
Kunal Choudhary <kunal13053@iiitd.ac.in>
2019-08-19 21:53:20 +0530

    Create 5_object_create.js

---
+++ OOP_JS/js/5_object_create.js
@@ -0,0 +1,26 @@
+// Create Object using object.create
+
+// Object Of Protos
+const bookProtos = {
+  getSummary: function() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  },
+  getAge: function() {
+    const years = new Date().getFullYear() - this.year;
+    return `${this.title} is ${years} years old.`;
+  }
+};
+
+// Create Object
+// const book1 = Object.create(bookProtos);
+// book1.title = 'Book One';
+// book1.author = 'John Doe';
+// book1.year = '2019';
+
+const book1 = Object.create(bookProtos, {
+  title: { value: 'Book Two' },
+  author: { value: 'John Doe' },
+  year: { value: '2019' }
+});
+
+console.log(book1);
